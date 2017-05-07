4df85df597055420eab49d351db2f0a1b2291888
Bianca Albuquerque <bianca_a.m@hotmail.com>
2017-05-06 21:06:50 -0300

    Criando Primeira Versão do Banco de Dados - Primeira versão da tabela 'aluno' - Primeira versão da tabela 'endereco'

--- database/migrations/2017_05_06_231303_create_aluno_table.php
+++ database/migrations/2017_05_06_231303_create_aluno_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateAlunoTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('aluno', function (Blueprint $table ) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('nome');
+            $table->string('matricula');
+            $table->float('nota');
+            $table->integer('endereco_id');
+            $table->foreign('endereco_id')->references('id')->on('endereco');
         });
     }
 
@@ -30,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('aluno');
     }
 }
